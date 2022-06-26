6f1f3dba3f35305e2f61ad8b613c6fa50b19be68
rajashekarnagalikar <rajashekarnagalikar@gmail.com>
2022-06-26 19:34:09 +0530

    logger and exception has been handled

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
+
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing exception module")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        #raise HousingException(e,sys) from e 
+        logging.info(housing.error_message)    
+        logging.info("we are testing logging module")
     return "starting the machine learning project"
 
 if __name__=="__main__":
---
+++ housing/exception/__init__.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+class HousingException(Exception):
+    
+    def __init__(self,error_message:Exception,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                        error_detail=error_detail
+                                                                        )
+
+
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
+        """
+        error message : Exception object
+        error_details : object of sys module 
+        """
+        _,_ ,exec_tb = error_detail.exc_info()
+        
+        line_number = exec_tb.tb_frame.f_lineno
+
+        file_name = exec_tb.tb_frame.f_code.co_filename
+
+        error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+
+        return error_message
+
+    def __str__(self):
+        """
+        This is the dunder method
+        """
+        return self.error_message
+
+    def __repr__(self) -> str:
+        return HousingException.__name_.str()
