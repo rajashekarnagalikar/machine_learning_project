708024456243dd49a5c802aa8f7cbb1a401f6759
rajashekarnagalikar <rajashekarnagalikar@gmail.com>
2022-06-26 12:28:29 +0530

    added housing folders and required files

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 # Declaring variables for setup functions
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "RAJ"
 DESCRIPTION = "This is the first FSDS Nov batch machine learning project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_lists()->List[str]:
@@ -18,13 +17,13 @@ def get_requirements_lists()->List[str]:
     mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_lists()
 )
