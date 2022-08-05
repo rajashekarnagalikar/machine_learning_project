9d8408da8c956f7cea0aad2630b86ffdf07c58f3
rajashekarnagalikar <rajashekarnagalikar@gmail.com>
2022-08-05 13:45:32 +0530

    Model trainer and Model evalution code updated

--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -14,7 +14,7 @@ from housing.entity.config_entity import DataIngestionConfig, ModelEvaluationCon
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
-# from housing.component.model_trainer import ModelTrainer
+from housing.component.model_trainer import ModelTrainer
 # from housing.component.model_evaluation import ModelEvaluation
 # from housing.component.model_pusher import ModelPusher
 import os, sys
@@ -61,8 +61,14 @@ class Pipeline:
             raise HousingException(e,sys) from e
         
 
-    def start_model_trainer(self):
-        pass
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                        data_transformation_artifact=data_transformation_artifact
+                                        )
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise HousingException(e,sys) from e
         
     def start_model_evaluation(self):
         pass
